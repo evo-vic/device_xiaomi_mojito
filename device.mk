04872aa156ad15924928bcbafa6d8e3035310ce1
romgharti <romgharti@gmail.com>
2024-04-23 04:39:45 +0545

    Revert "mojito: Switch to android.hardware.usb@1.3-service.dual_role_usb"
    This reverts commit 454f421c541c9d66a2f6a9ef24fb8c9157cd100e.

--- device.mk
+++ device.mk
@@ -497,7 +497,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.0-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
